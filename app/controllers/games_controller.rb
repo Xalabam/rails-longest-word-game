94367db43da2376c4fb835db5fe8eaeadf348183
Alex Tchaikovsky <xalabam94@icloud.com>
2020-11-16 17:03:39 +0800

    Created page 'new' with 10 random letters and form. Missing a score page

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      letter = ('A'..'Z').to_a.sample
+      @letters << letter
+    end
+  end
+
+  def score
+    raise
+  end
+end
